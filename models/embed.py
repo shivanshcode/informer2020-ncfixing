49f0027d42c1ec66ad7c269c227c6926bb577eb6
shivanshcode <70197059+shivanshcode@users.noreply.github.com>
2025-04-14 13:59:11 +0530

    Update embed.py

--- models/embed.py
+++ models/embed.py
@@ -446,7 +446,7 @@ class TimeFeatureEmbedding(nn.Module):
 class DataEmbedding(nn.Module):
     def __init__(self, c_in, d_model, embed_type='fixed', freq='h', dropout=0.1):
         super(DataEmbedding, self).__init__()
-
+        print(f'cin in DataEmbedding: {c_in}', flush =True)
         self.value_embedding = TokenEmbedding(c_in=c_in, d_model=d_model)
         self.position_embedding = PositionalEmbedding(d_model=d_model)
         self.temporal_embedding = TemporalEmbedding(d_model=d_model, embed_type=embed_type, freq=freq) if embed_type!='timeF' else TimeFeatureEmbedding(d_model=d_model, embed_type=embed_type, freq=freq)
