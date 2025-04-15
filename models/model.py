f184d4e94e6ee4043a5e217523de58a1bb1cee54
shivanshcode <70197059+shivanshcode@users.noreply.github.com>
2025-04-15 14:52:37 +0530

    Update model.py

--- models/model.py
+++ models/model.py
@@ -68,10 +68,13 @@ class Informer(nn.Module):
                 enc_self_mask=None, dec_self_mask=None, dec_enc_mask=None):
         enc_out = self.enc_embedding(x_enc, x_mark_enc)
         enc_out, attns = self.encoder(enc_out, attn_mask=enc_self_mask)
+        print(f'Encoder Output: {enc_out.size()}', flush=True)
 
         dec_out = self.dec_embedding(x_dec, x_mark_dec)
         dec_out = self.decoder(dec_out, enc_out, x_mask=dec_self_mask, cross_mask=dec_enc_mask)
         dec_out = self.projection(dec_out)
+        print(f'Encoder Output: {enc_out.size()}', flush=True)
+
         #print(f'Decoder Output Shape from model.py/Informer: {dec_out.shape}', flush=True)
         # dec_out = self.end_conv1(dec_out)
         # dec_out = self.end_conv2(dec_out.transpose(2,1)).transpose(1,2)
